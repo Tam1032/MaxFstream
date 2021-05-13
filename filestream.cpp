837f3c888a54889c126339d3891597048240d9a9
Tam10324 <tamnm1032@gmail.com>
2021-05-13 20:40:19 +0700

    1st commit

---
+++ filestream.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    int n;
+    string name;
+    ifstream fi("input.txt");   // mo file de doc
+    ofstream fo("output.txt");  // mo file de ghi
+
+    fi >> n;                  // doc 1 so tu file
+    getline(fi, name);          // loai bo dau xuong dong sau khi doc so n
+
+    for (int i = 0; i <n; i++)
+    {
+        getline(fi, name);      // doc 1 dong tu file
+        fo << (i+1) << "." << name << '\n';     // ghi 1 dong ra file
+    }
+
+    fi.close();
+    fo.close();
+
+    return 0;
+}
